131dc37506c16c93adc4e9acb70de799d24cdc45
tamim <tamimarefinanik@gmail.com>
2024-03-14 13:15:42 +0600

    loading images and view

--- App.jsx
+++ App.jsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello world from react native!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world from react native</Text>
+      <Image
+        style={styles.img}
+        source={{
+          uri: 'https://randomuser.me/api/portraits/men/22.jpg',
+        }}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'darkslateblue', 
+    fontSize: 40
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100/2
+  }
+});
 
 export default App;
