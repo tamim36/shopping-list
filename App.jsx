b803cb7a329e84ced7394622aee28cb7c3b8a1b0
tamim <tamimarefinanik@gmail.com>
2024-03-18 10:29:35 +0600

    adding header component and use props

--- App.jsx
+++ App.jsx
@@ -1,34 +1,16 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world from react native</Text>
-      <Image
-        style={styles.img}
-        source={{
-          uri: 'https://randomuser.me/api/portraits/men/22.jpg',
-        }}
-      />
-    </View>
+    <Header />
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue', 
-    fontSize: 40
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100/2
+    flex: 1
   }
 });
 
---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text} > { title } </Text>
+    </View>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Shopping List',
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        padding: 15,
+        backgroundColor: 'darkslateblue'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 23,
+        textAlign: 'center'
+    }
+});
